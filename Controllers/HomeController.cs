1f82a2ba246164048ec7f7b11764fede3cb99bfd
Simon Moreau <simon@bim42.com>
2018-01-10 10:12:25 +0100

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -35,6 +35,8 @@ namespace bimsyncManagerAPI.Controllers
         {
             try
             {
+                Configuration["client_id"] = Environment.GetEnvironmentVariable("APPSETTING_client_id");
+                Configuration["client_secret"] = Environment.GetEnvironmentVariable("APPSETTING_client_secret");
                 string client_id = Configuration["client_id"];
                 string client_secret = Configuration["client_secret"];
                 return client_id + " - " + client_secret;
