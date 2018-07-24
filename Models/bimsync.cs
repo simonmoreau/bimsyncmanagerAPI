87ed7a9111c926b603fa49e21d32a0f2f88f074e
simonmoreau <simon@bim42.com>
2018-07-24 17:48:41 +0200

    Allows for null id

--- Models/bimsync.cs
+++ Models/bimsync.cs
@@ -46,7 +46,7 @@ namespace bimsyncManagerAPI.bimsync
     public class IfcElement
     {
         public string revisionId { get; set; }
-        public long objectId { get; set; }
+        public long? objectId { get; set; }
         public string ifcType { get; set; }
         public object attributes { get; set; }
         public IfcElement type { get; set; }
